841bfbd56917d65d0a63d21628974f23c8b22e7c
ArchyScript <yungdansa@gmail.com>
2024-10-04 22:57:24 +0100

    app scafoid

---
+++ src/components/global/Icon.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+import { ref, watchEffect, onMounted } from 'vue'
+import type { IconType } from '@/types'
+// Define the props for the component
+interface Props {
+  name: string
+  filled?: boolean
+  size?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  filled: true,
+  size: 24
+})
+
+const hasStroke = ref(false)
+const icon = ref<string>('')
+
+// Fetch the SVG icon dynamically
+const fetchIcon = async () => {
+  try {
+    // Import SVG files from the `@/assets/icons` folder
+    const iconsImport = import.meta.glob('@/assets/icons/**/*.svg', {
+      eager: false,
+      query: '?raw',
+      import: 'default'
+    })
+
+    const iconPath = `/src/assets/icons/${props.name}.svg`
+
+    if (!iconsImport[iconPath]) {
+      throw new Error(`Icon "${props.name}" not found`)
+    }
+
+    const rawIcon = await iconsImport[iconPath]()
+    const iconSvgContent = rawIcon as string
+
+    // Check if the icon contains a stroke
+    hasStroke.value = iconSvgContent.includes('stroke')
+    icon.value = iconSvgContent
+  } catch (error) {
+    // Handle the case where the icon is not found
+    throw new Error(`Icon "${props.name}" not found`)
+  }
+}
+
+// Fetch the icon when the component is mounted
+onMounted(() => fetchIcon())
+// Re-fetch the icon whenever the name prop changes
+watchEffect(() => fetchIcon())
+</script>
+
+<template>
+  <!-- Render the loaded SVG content -->
+  <span
+    class="base-icon"
+    :class="{ 'icon--fill': filled, 'icon--stroke': hasStroke && !filled }"
+    v-html="icon"
+    :style="{ width: `${props.size}px`, height: `${props.size}px` }"
+  />
+</template>
+
+<style scoped>
+/* Base styling for the icon */
+.base-icon {
+  line-height: 0;
+}
+
+/* Apply fill color to the icon */
+.base-icon.icon--fill,
+.base-icon.icon--fill * {
+  fill: currentColor !important;
+}
+
+/* Apply stroke color to the icon */
+.base-icon.icon--stroke,
+.base-icon.icon--stroke * {
+  stroke: currentColor !important;
+}
+</style>
---
+++ src/components/layout/LeftPanel.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="h-full bg-[#111B21]">
+    <!-- <transition name="fade" mode="out-in">
+      <Chats v-if="activeTab === 'chats'" key="chats" />
+    </transition>
+ 
+    <transition name="fade" mode="out-in">
+      <Status v-if="activeTab === 'status'" key="status" />
+    </transition>
+ 
+    <transition name="fade" mode="out-in">
+      <Channels v-if="activeTab === 'channels'" key="channels" />
+    </transition>
+ 
+    <transition name="fade" mode="out-in">
+      <Communities v-if="activeTab === 'communities'" key="communities" />
+    </transition>
+ 
+    <transition name="fade" mode="out-in">
+      <Settings v-if="activeTab === 'settings'" key="settings" />
+    </transition>
+ 
+    <transition name="fade" mode="out-in">
+      <Profile v-if="activeTab === 'profile'" key="profile" />
+    </transition> -->
+
+    <Chats v-if="activeTab === 'chats'" />
+    <Status v-if="activeTab === 'status'" />
+    <Channels v-if="activeTab === 'channels'" />
+    <Communities v-if="activeTab === 'communities'" />
+    <Settings v-if="activeTab === 'settings'" />
+    <Profile v-if="activeTab === 'profile'" />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import {
+  Chats,
+  Communities,
+  Status,
+  Settings,
+  Channels,
+  Profile
+} from '@/components/LeftPanel/index.ts'
+
+import { useAppStore } from '@/stores/app'
+
+const activeTab = computed(() => useAppStore().activeTab)
+</script>
+
+<style scoped>
+.relative:hover .opacity-0 {
+  opacity: 1;
+}
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 4s ease;
+}
+.slide-enter,
+.slide-leave-to {
+  transform: translateX(100%);
+}
+</style>
